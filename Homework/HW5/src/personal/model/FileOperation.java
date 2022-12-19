4c53612f34f5e4e28925245e0ca263ac96851321
MaksWoropaew <maks.woropaew@mail.ru>
2022-12-19 12:44:06 +0300

    add hw5

---
+++ Homework/HW5/src/personal/model/FileOperation.java
@@ -0,0 +1,9 @@
+package Homework.HW5.src.personal.model;
+
+import java.util.List;
+
+public interface FileOperation {
+    List<String> readAllLines();
+
+    void saveAllLines(List<String> lines);
+}
---
+++ Homework/HW5/src/personal/views/Commands.java
@@ -0,0 +1,11 @@
+package Homework.HW5.src.personal.views;
+
+public enum Commands {
+    NONE,
+    READ,
+    CREATE,
+    UPDATE,
+    LIST,
+    DELETE,
+    EXIT
+}
